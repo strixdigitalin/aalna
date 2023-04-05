ddc01a52dcf4c8f2657401e7a7b5fc1fd64af4b7
Piyush-Ratlani <69811087+Piyush-Ratlani@users.noreply.github.com>
2023-04-05 18:23:02 +0530

    send url

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -315,11 +315,13 @@ module.exports.ccavenuerequesthandler = (request, response) => {
       '"><input type="hidden" name="access_code" id="access_code" value="' +
       accessCode +
       '"><script language="javascript">document.redirect.submit();</script></form>';
+    url = `https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction&encRequest=${encRequest}&access_code=${accessCode}`;
   });
 
   request.on("end", function () {
     response.writeHeader(200, { "Content-Type": "text/html" });
-    response.write(formbody);
+    // response.write(formbody);
+    response.write(url);
     response.end();
   });
   return;
