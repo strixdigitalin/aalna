b6ece3369d72ae4cc76caebbbe5882cf19b56c60
lakheraakshay <akshay.lakhera@easystepin.com>
2023-04-26 12:24:57 +0530

    changes doen

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -7,78 +7,87 @@ const {
   shortIdChar,
 } = require("../utility");
 const shortid = require("shortid");
+const uploadOnCloudinary = require("../middlewares/Cloudinary");
 
-module.exports.addBlog_post = (req, res) => {
+module.exports.addBlog_post = async (req, res) => {
   const { title, content, displayImage } = req.body;
   const blogId = shortid.generate(shortIdChar);
-  if (!title || !content || !displayImage || displayImage.length == 0)
-    return errorRes(res, 400, "All fields are required.");
+  if (!req?.files) return errorRes(res, 400, " Blog Image is required.");
+  if (!req?.files?.image) return errorRes(res, 400, " Blog Image is required.");
+  if (req?.files?.image?.length == 0)
+    return errorRes(res, 400, " Blog Image is required.");
+  if (!title || !content) return errorRes(res, 400, "All fields are required.");
   else {
+    const imageurl1 = await uploadOnCloudinary(req.files.image[0]);
     const blog = new Blog({
       title,
       content,
-      displayImage,
+      displayImage: [{ url: imageurl1 }],
       // blogCategory,
       blogId,
     });
     blog
       .save()
-      .then(savedBlog => {
+      .then((savedBlog) => {
         if (!savedBlog)
           return errorRes(res, 500, "Internal server error. Please try again.");
         else {
           Blog.findById(savedBlog._id)
             //   .populate('blogCategory', '_id name displayImage')
-            .then(result =>
+            .then((result) =>
               successRes(res, {
                 blog: result,
                 message: "Blog added successfully.",
               })
             )
-            .catch(err => internalServerError(res, err));
+            .catch((err) => internalServerError(res, err));
         }
       })
-      .catch(err => internalServerError(res, err));
+      .catch((err) => internalServerError(res, err));
   }
 };
 
-module.exports.editBlog_post = (req, res) => {
+module.exports.editBlog_post = async (req, res) => {
   const { _id } = req.params;
   const { title, content, displayImage } = req.body;
   const updates = {};
-
+  console.log(req.body, "<<<thisiseditblog");
   if (title) {
     updates.title = title;
   }
   if (content) {
     updates.content = content;
   }
+  let img = displayImage;
+  if (req?.files?.image?.length > 0) {
+    img = [{ url: await uploadOnCloudinary(req.files.image[0]) }];
+  }
   if (displayImage && displayImage.length !== 0) {
-    updates.displayImage = displayImage;
+    updates.displayImage = img;
   }
 
   if (Object.keys(updates).length == 0)
     return errorRes(res, 400, "No updates made.");
 
   Blog.findByIdAndUpdate(_id, updates, { new: true, runValidators: true })
-    .then(updatedBlog =>
+    .then((updatedBlog) =>
       successRes(res, { updatedBlog, message: "Blog updated successfully." })
     )
-    .catch(err => internalServerError(res, err));
+    .catch((err) => internalServerError(res, err));
 };
 
 module.exports.getAllBlogs_get = (req, res) => {
   Blog.find()
     .sort("-createdAt")
-    .then(blogs => successRes(res, { blogs }))
-    .catch(err => internalServerError(res, err));
+    .then((blogs) => successRes(res, { blogs }))
+    .catch((err) => internalServerError(res, err));
 };
 
 module.exports.deleteBlog_delete = (req, res) => {
   const { _id } = req.params;
 
   Blog.findByIdAndDelete(_id)
-    .then(deletedBlog => {
+    .then((deletedBlog) => {
       if (!deletedBlog) return errorRes(res, 404, "Blog does not exist.");
       else
         return successRes(res, {
@@ -86,5 +95,5 @@ module.exports.deleteBlog_delete = (req, res) => {
           message: "Blog deleted successfully.",
         });
     })
-    .catch(err => internalServerError(res, err));
+    .catch((err) => internalServerError(res, err));
 };
--- src/controllers/site_trending_product.controller.js
+++ src/controllers/site_trending_product.controller.js
@@ -1,27 +1,65 @@
 const mongoose = require("mongoose");
 const Site_Trending_Product = mongoose.model("Site_Trending_Product");
 const { errorRes, internalServerError, successRes } = require("../utility");
+const uploadOnCloudinary = require("../middlewares/Cloudinary");
 
 module.exports.addTrendingProduct_post = async (req, res) => {
-  const { productImages } = req.body;
+  const { prevImages } = req.body;
+  let prevImagesParsed = JSON.parse(prevImages);
   const result = [];
 
-  if (!productImages || productImages?.length == 0)
+  if (!prevImages || prevImages?.length == 0)
     return errorRes(res, 400, "Banner list empty.");
 
   try {
     await Site_Trending_Product.deleteMany({});
+    console.log(req.files.image);
+    if (req?.files?.image?.length) {
+      await Promise.all(
+        req?.files?.image?.map(async (item, index) => {
+          const imageurl1 = await uploadOnCloudinary(req.files.image[index]);
+          const productImage = new Site_Trending_Product({
+            productImage: { url: imageurl1 },
+          });
+          console.log(productImage, "<<<thisisprevimage");
 
-    await Promise.all(
-      productImages.map(async item => {
-        const productImage = new Site_Trending_Product(item);
-        await productImage
-          .save()
-          .then(productImage => result.push(productImage));
-      })
-    );
+          await productImage
+            .save()
+            .then((productImage) => result.push(productImage));
+        })
+      );
+    }
+    if (prevImagesParsed.length > 0) {
+      await Promise.all(
+        prevImagesParsed?.map(async (item) => {
+          // const imageurl1 = await uploadOnCloudinary(req.files.image[index]);
+          const productImage = new Site_Trending_Product({
+            productImage: { url: item },
+          });
+          console.log(productImage, "<<<thisisprevimage");
+          await productImage
+            .save()
+            .then((productImage) => result.push(productImage));
+        })
+      );
+    }
 
-    if (result.length == 0) return errorRes(res, 400, "Error saving banners.");
+    console.log(prevImagesParsed, "<<<thisisprevImages");
+
+    // await Promise.all(
+    //   productImages.map(async item => {
+    //     const productImage = new Site_Trending_Product(item);
+    //     await productImage
+    //       .save()
+    //       .then(productImage => result.push(productImage));
+    //   })
+    // );
+
+    if (result.length == 0)
+      return successRes(res, {
+        site_trending_product: result,
+        message: "Trending products saved successfully.",
+      });
     else
       return successRes(res, {
         site_trending_product: result,
@@ -34,6 +72,8 @@ module.exports.addTrendingProduct_post = async (req, res) => {
 
 module.exports.getAllTrendingProduct_get = (req, res) => {
   Site_Trending_Product.find()
-    .then(site_trending_products => successRes(res, { site_trending_products }))
-    .catch(err => internalServerError(res, err));
+    .then((site_trending_products) =>
+      successRes(res, { site_trending_products })
+    )
+    .catch((err) => internalServerError(res, err));
 };
